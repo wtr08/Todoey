28a369169949a9e08a5a5bf5d0bf5567c5556aed
wouter <wouter_07@live.nl>
2018-03-30 08:41:34 +0200

    Intermediate commit for Class inheritance

--- Todoey/Controllers/CategoryViewController.swift
+++ Todoey/Controllers/CategoryViewController.swift
@@ -2,47 +2,48 @@
 //  CategoryViewController.swift
 //  Todoey
 //
-//  Created by Wouter van Rijmenam on 05/03/2018.
-//  Copyright © 2018 Stackmont. All rights reserved.
+//  Created by Angela Yu on 01/12/2017.
+//  Copyright © 2017 Angela Yu. All rights reserved.
 //
 
 import UIKit
-import CoreData
 import RealmSwift
 
-class CategoryViewController: UITableViewController {
+class CategoryViewController: SwipeTableViewController {
     
     let realm = try! Realm()
     
-    // Variables
-    var categories : Results<Category>?
-
+    var categories: Results<Category>?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         loadCategories()
+        
     }
-   
-    // MARK : - TableView Datasource Methods
+    
+    //MARK: - TableView Datasource Methods
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
         return categories?.count ?? 1
+        
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
         
-        cell.textLabel?.text = categories?[indexPath.row].name ?? "No Categories Added yet"
-        
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)
+        cell.textLabel?.text = categories?[indexPath.row].name ?? "No Categories Added Yet"
+        self.tableView.rowHeight = 80.0
         return cell
         
     }
     
-    // MARK: - TableView Delegate Methods
+
+    //MARK: - TableView Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: "goToItems", sender: self)
-        
-        
-        
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -53,14 +54,52 @@ class CategoryViewController: UITableViewController {
         }
     }
     
-    // MARK: - Add New Categories
+    //MARK: - Data Manipulation Methods
+    
+    func save(category: Category) {
+        do {
+            try realm.write {
+                realm.add(category)
+            }
+        } catch {
+            print("Error saving category \(error)")
+        }
+        
+        tableView.reloadData()
+        
+    }
+    
+    func loadCategories() {
+        
+        categories  = realm.objects(Category.self)
+        
+        tableView.reloadData()
+        
+    }
+    
+    //Mark: - Delete data form swipe
+    override func updateModel(at indexPath: IndexPath) {
+        if let categoryForDeletion = categories?[indexPath.row] {
+            do{
+                try self.realm.write {
+                    self.realm.delete(categoryForDeletion)
+                }
+            } catch {
+                print("Delete item occured an error: \(error)")
+            }
+        }
+        
+    }
+    
+    //MARK: - Add New Categories
+    
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
         var textField = UITextField()
         
-        let alert = UIAlertController(title: "Add New Todoey category", message: "", preferredStyle: .alert)
+        let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert)
         
-        let action = UIAlertAction(title: "Add Category", style: .default) { (action) in
-            // what will happen once the user clicks the add item button on our UIAlert
+        let action = UIAlertAction(title: "Add", style: .default) { (action) in
             
             let newCategory = Category()
             newCategory.name = textField.text!
@@ -69,39 +108,19 @@ class CategoryViewController: UITableViewController {
             
         }
         
-        alert.addTextField { (alertTextField) in
-            alertTextField.placeholder = "Create new category"
-            textField = alertTextField
+        alert.addAction(action)
+        
+        alert.addTextField { (field) in
+            textField = field
+            textField.placeholder = "Add a new category"
         }
         
-        alert.addAction(action)
         present(alert, animated: true, completion: nil)
         
     }
     
-   
-    // ** MARK: - Data Manipulation Methods
     
     
-    func save(category:Category) {
-        do {
-            try realm.write {
-                realm.add(category)
-            }
-        } catch {
-            print("Error saving context, \(error)")
-        }
-        tableView.reloadData()
-    }
-    
-
-    func loadCategories() {
-
-        categories = realm.objects(Category.self)
-        
-        tableView.reloadData()
-
-    }
     
     
 }
